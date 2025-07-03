1e9817f78bd7900c14d9334e725966ac0a9c277e
Suyash Pandey <suyashpandey607@gmail.com>
2025-07-03 19:01:34 +0530

    Refactor server startup to use async function for database connection

--- server/src/server.js
+++ server/src/server.js
@@ -5,12 +5,21 @@ import { connectDB } from "./configs/db.js";
 const app = express();
 app.use(express.json());
 
-connectDB();
-
 app.get("/", (req, res) => {
   res.send("Hello from Server!");
 });
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
