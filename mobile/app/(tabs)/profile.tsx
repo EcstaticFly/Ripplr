5d0ba1f631d366e016e0dc3d23c168a6b4cc3cb3
Suyash Pandey <suyashpandey607@gmail.com>
2025-08-01 20:41:02 +0530

    [feat]: Add Tab navigation in homescreen to navigate between home, search, notifications, messages, profile. - Created a custom hook for signOut implementation. - Temporarily positioned the custom SignOutBtn component in homeScreen.

---
+++ mobile/app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const ProfileScreen = () => {
+  return (
+    <SafeAreaView className="flex-1">
+      <Text>ProfileScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default ProfileScreen;
---
+++ mobile/app/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SearchScreen = () => {
+  return (
+    <SafeAreaView className="flex-1">
+      <Text>SearchScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SearchScreen;
